f9604a7183bd824fc9d48fe3bb3f40e26916dd2d
Tyler Davis <Ryan.Tyler.Davis@gmail.com>
2017-03-03 15:52:14 -0700

    api cards controller

---
+++ app/controllers/api/cards_controller.rb
@@ -0,0 +1,45 @@
+class Api::CardsController < ApplicationController
+  before_action :set_list
+  before_action :set_card, only: [:updade, :destroy]
+
+  def index
+    render json: @list.cards
+  end
+
+  def create
+    card = @list.cards.new(card_params)
+    if card.save
+      render json: card
+    else
+      render json: { errors: card.errors }, status: 401
+    end
+  end
+
+  def update
+    if @card.update(card_params)
+      render json: @card
+    else
+      render json: { errors: card.errors }, status: 401
+    end
+  end
+
+  def destroy
+    @card.destroy
+    head :no_content
+  end
+
+  private
+
+    def set_list
+      @list = List.find(params[:list_id])
+    end
+
+    def set_card
+      @card = @list.cards.find(params[:id])
+    end
+
+    def card_params
+      params.require(:card).permit(:body)
+    end
+
+end
